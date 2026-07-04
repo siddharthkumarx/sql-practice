f83a51d90835f08b1a8f4cc2d08613797d44bd8f
Siddharth Kumar <siddharth.kumar.career@gmail.com>
2026-07-04 18:31:37 +0530

    Set up sales database and modify stores_new table
    Created a new database and tables for sales, inserted records, and altered a column in the stores_new table.

--- sql-ansh-basic.sql
+++ sql-ansh-basic.sql
@@ -1,1 +1,40 @@
 -- SQL BEGINEER/ BASIC
+-- CHAPTER 1
+-- Create Database
+CREATE DATABASE sales;
+
+-- Create Table
+CREATE TABLE stores
+(
+	store_id INT,
+    store_name VARCHAR(200)
+);
+
+-- Insert Some Records
+INSERT INTO stores(store_id)
+VALUES
+(3);
+
+-- Create Table
+CREATE TABLE stores_new
+(
+	store_id INT UNIQUE,
+    store_name VARCHAR(200) NOT NULL
+);
+
+-- Insert Some Data
+INSERT INTO stores_new
+VALUES
+(1,"store_xyz");
+
+
+-- ALTER COMMAND
+ALTER TABLE stores_new
+RENAME COLUMN store_city to store_location;
+
+
+
+
+
+
+
