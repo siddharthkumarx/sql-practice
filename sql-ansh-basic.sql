240150e4394d788b9fd636c95dfecff4fba60172
Siddharth Kumar <siddharth.kumar.career@gmail.com>
2026-07-09 16:02:52 +0530

    Add SQL queries for various join types and unions

--- sql-ansh-basic.sql
+++ sql-ansh-basic.sql
@@ -127,7 +127,71 @@ GROUP BY
 HAVING
 	avg_price > 500;
 ----------------------CHAPTER 3 COMPLETE-------------------------------------------------
-    
+#CHAPTER 4
+-- JOINS 
+-- INNER JOIN
+SELECT 
+	*
+FROM 
+	orders o 
+INNER JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id;
+
+-- LEFT JOIN
+SELECT 
+	*
+FROM 
+	orders o 
+LEFT JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id;
+
+-- RIGHT JOIN
+SELECT 
+	*
+FROM 
+	orders o 
+RIGHT JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id;
+        
+-- FULL JOIN (Not Supported) in MYSQL
+SELECT 
+	*
+FROM 
+	orders o 
+FULL JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id;
+        
+        
+-- UNIONS
+SELECT 
+	*
+FROM 
+	orders o 
+LEFT JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id
+
+UNION
+
+SELECT 
+	*
+FROM 
+	orders o 
+RIGHT JOIN 
+	customers c 
+    ON 
+		o.cust_id = c.id;
+	
+----------------------------CHAPTER 4 COMPLETE------------------------------------------------
     
 
 
