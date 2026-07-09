43c5c3c2b782bf810f84c7e1371e4c62eef344d3
Siddharth Kumar <siddharth.kumar.career@gmail.com>
2026-07-09 13:42:28 +0530

    Update sql-ansh-basic.sql

--- sql-ansh-basic.sql
+++ sql-ansh-basic.sql
@@ -1,4 +1,5 @@
 -- SQL BEGINEER/ BASIC
+
 -- CHAPTER 1
 -- Create Database
 CREATE DATABASE sales;
@@ -33,8 +34,100 @@ ALTER TABLE stores_new
 RENAME COLUMN store_city to store_location;
 
 
+-- CHAPTER 3
+
+-- FIRST SELECT 
+SELECT * FROM dim_customer;
 
 
+-- LIMIT 
+SELECT 
+	customer_id,
+	email 
+FROM 
+	dim_customer
+LIMIT 15;
 
+
+-- WHERE [CONDITION]
+-- 1
+SELECT 
+	* 
+FROM 
+	dim_customer 
+WHERE 
+	gender = 'F';
+
+-- 2 (AND/OR)
+SELECT 
+	* 
+FROM 
+	dim_customer 
+WHERE 
+	(gender = 'F') AND ((country = 'France') OR (join_date > '2022-01-01'));
+    
+    
+-- LIKE = kaisa result chaiye . % reminder indicate. T% means first t then baki kuch bhi
+-- 1)
+SELECT 
+	* 
+FROM 
+	dim_customer
+WHERE 
+	first_name LIKE 'T%';
+-- 2)
+SELECT 
+	* 
+FROM 
+	dim_customer
+WHERE 
+	first_name LIKE 'T__f%y';
+
+    
+-- Sorting
+SELECT 
+	* 
+FROM 
+	dim_product
+ORDER BY 
+	unit_price DESC 
+LIMIT 3;
+
+
+-- ALIAS
+SELECT 
+	product_key,
+    product_id,
+    product_name AS 'product name',
+    category
+FROM 
+	dim_product;
+
+
+-- GROUPING
+-- 1
+SELECT 
+	category,
+    avg(unit_price) AS avg_price,
+    sum(unit_price) AS total_price
+FROM 
+	dim_product
+GROUP BY 
+	category;
+    
+-- 2
+SELECT 
+	category,
+    avg(unit_price) AS avg_price,
+    sum(unit_price) AS total_price
+FROM 
+	dim_product
+GROUP BY 
+	category
+HAVING
+	avg_price > 500;
+----------------------CHAPTER 3 COMPLETE-------------------------------------------------
+    
+    
 
 
